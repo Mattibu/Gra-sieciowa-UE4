1ae2ca02e65f6682ebf0c0ddd8c2c2c8f35e54f2
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2020-11-14 15:04:37 +0100

    Fixed .gitignore issues

--- GameServer/GameServerExec/GameServerExec.cpp
+++ GameServer/GameServerExec/GameServerExec.cpp
@@ -1,6 +1,13 @@
 #include "ServerLog.h"
+#include "BufferPool.h"
+#include "WinTCPMultiClientServer.h"
+
+using namespace spacemma;
 
 int main()
 {
+    const unsigned char MAX_CLIENTS = 8U;
+    BufferPool bufferPool(1024 * 1024);
+    WinTCPMultiClientServer server(bufferPool, MAX_CLIENTS);
     return 0;
 }
