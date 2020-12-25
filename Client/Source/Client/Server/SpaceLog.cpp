86b9fd9cf5519bd8d66d6e27d7d4385df0cc2f4d
Doctor Ned <1933864+Doctor-Ned@users.noreply.github.com>
2020-12-25 20:19:20 +0100

    Added file sink back

--- Client/Source/Client/Server/SpaceLog.cpp
+++ Client/Source/Client/Server/SpaceLog.cpp
@@ -19,7 +19,7 @@ spdlog::logger* spacemma::SpaceLog::getLogger()
             SPACEMMA_LOG_FILENAME, SPACEMMA_LOG_FILE_SIZE, SPACEMMA_LOG_MAX_FILES);
         auto ueSink = std::make_shared<UEConsoleSink_mt>();
         ueSink->set_pattern("[%Y-%m-%d %T.%e] [%s:%#] %v");
-        spdlog::sinks_init_list sinkList{ ueSink };
+        spdlog::sinks_init_list sinkList{ ueSink, rotatingFileSink };
         logger = std::make_shared<spdlog::logger>(SPACEMMA_LOG_NAME, sinkList);
         logger->set_level(static_cast<spdlog::level::level_enum>(SPACEMMA_LOG_LEVEL));
         set_default_logger(logger);
